4e27c48c5ed236e71290ea19bef6e5bf9bc7d89b
Saurav <sauravks2010@gmail.com>
2025-02-25 15:40:25 +0530

    bbb

--- src/routes/v1/driver.route.js
+++ src/routes/v1/driver.route.js
@@ -18,8 +18,8 @@ const driverSchema = Joi.object({
 
 // Create a Driver
 router.post('/', async (req, res) => {
-  const { error } = driverSchema.validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  // const { error } = driverSchema.validate(req.body);
+  // if (error) return res.status(400).send(error.details[0].message);
 
   const driver = new Driver(req.body);
   try {
